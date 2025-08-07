1978126f5bb2529991b16e878638a98e7a6bc9a9
Rakesh Kumar <Rakeshkumarr60034@gmail.com>
2025-08-07 14:09:37 +0500

    Testing is completed

--- testing/app.js
+++ testing/app.js
@@ -1,20 +1,18 @@
-const fs = require('fs');
+const http = require('http');
 
-fs.readFile('file.txt','utf-8',(err,data) => {
-   if (err) {
-    console.log(err);
-   }else{
-    console.log(data.toUpperCase());
-   }
-   const upper = data.toUpperCase();
-
-    fs.writeFile('output.txt',upper,(err) => {
-     if (err) {
-        console.log(err);
-     }else{
-        console.log("Appended");
+const server = http.createServer((req,res) => {
+   const user = {
+      "name": "John Doe",
+      "age": 30,
+      "profession": "Developer"
      }
-    })
-
+     if (req.url === '/api/user') {
+       res.end(JSON.stringify(user))
+     }else{
+      res.end('Page not found')
+     }
 })
 
+server.listen(3000,() => {
+   console.log('Server is ruining on port 3000');
+})
